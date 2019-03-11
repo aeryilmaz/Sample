c609bf47de4280fdc3d49ab5dacdd944152c5e59
Abdurrahman ERYILMAZ <32570998+aeryilmaz@users.noreply.github.com>
2019-03-11 18:57:59 +0300

    Java Lib UTİL ArrayUtil Console CountdownTimer MutableIntNumber Pair StringUtil IntNumber datetime function

--- src/org/csystem/app/App.java
+++ src/org/csystem/app/App.java
@@ -12,7 +12,7 @@ class App {
     public static void main(String [] args)
     {
         RandomIntGenerator intGenerator = new RandomIntGenerator(5, 1, 101);
-
+    //..
         for (int val : intGenerator)
             System.out.printf("%d ", val);
 
